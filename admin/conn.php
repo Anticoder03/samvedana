fbde6cc2ff7a07bc6abcb4757b1730789ea9bb50
Anticoder03 <ap5381545@gmail.com>
2025-01-27 21:39:34 +0530

    Your commit message here

--- admin/conn.php
+++ admin/conn.php
@@ -1,10 +1,16 @@
-
 <?php
+$servername = "sql5.freesqldatabase.com";
+$username = "sql5759816";
+$password = "ABbGyugrHd";
+$dbname = "sql5759816";
+$port = 3306;
 
 // Create connection
-$conn = new mysqli('localhost', 'root', '', 'genral_labs');
-
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// echo "Connected successfully";
+?>
--- conn.php
+++ conn.php
@@ -1,8 +1,14 @@
 
 <?php
 
+$servername = "sql5.freesqldatabase.com";
+$username = "sql5759816";
+$password = "ABbGyugrHd";
+$dbname = "sql5759816";
+$port = 3306;
+
 // Create connection
-$conn = new mysqli('localhost', 'root', '', 'genral_labs');
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
